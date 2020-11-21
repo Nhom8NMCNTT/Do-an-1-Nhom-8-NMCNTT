9fd48c10e1ba3d51e0f453683d656a74e06ad2b1
TuanKhaisieuvippro123 <74805916+TuanKhaisieuvippro123@users.noreply.github.com>
2020-11-21 12:01:46 +0700

    tìm phần tử thứ k của dãy fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+unsigned long long fibo(int n)
+{
+	if (n == 0 || n == 1) // điều kiện dừng
+		return n; // nếu n bằng 0 hoặc bằng 1 thi trả về n
+	else
+		return (fibo(n - 2) + fibo(n - 1));
+}
+
+void main()
+{
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << fibo(n);
+}

a28216f8991dbdfb8e0d4bcf9c895b1cb872e9ea
Nhom8NMCNTT <74805882+Nhom8NMCNTT@users.noreply.github.com>
2020-11-21 14:45:39 +0700

    Tính fibonacci(k)

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,6 +2,5 @@ unsigned long long fibo(long long n)
 {
 	if (n == 0 || n == 1) // điều kiện dừng
 		return n; // nếu n bằng 0 hoặc bằng 1 thi trả về n
-	else
-		return (fibo(n - 2) + fibo(n - 1));
+	else return (fibo(n - 2) + fibo(n - 1));
 }

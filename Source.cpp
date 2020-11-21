491c14f1726fca713337e08f283fe57c36e285e8
anhhutran <74806073+anhhutran@users.noreply.github.com>
2020-11-21 12:15:49 +0700

    tính UCLN ,BCNN của 2 số nguyên được nhập

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+int UCLN(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    return a;
+}
+
+int BCNN(int a, int b)
+{
+    int kq = UCLN(a, b);
+    return a * b / kq;
+}

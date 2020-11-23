799c3ef9db0ab64c6dfe690d16f8f6cb7f65c581
vothanhlam3042002 <lamthanhvo3042002@gmail.com>
2020-11-23 08:12:27 +0700

    Create phantichthuasonguyento.cpp

---
+++ phantichthuasonguyento.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void phantichthuasonguyento(long long n)
+{
+	long int d = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		if (n == 0)
+			break;
+		while (n % i == 0)
+		{
+			d++;
+			n /= i;
+		}
+		if (d != 0)
+			cout << i << "^" << d << "*";
+		d = 0;
+	}
+	return;
+}

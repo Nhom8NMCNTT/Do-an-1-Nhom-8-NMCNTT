062cb08eb18b01b0db3d1a788142308a60198e76
Nhom8NMCNTT <74805882+Nhom8NMCNTT@users.noreply.github.com>
2020-12-15 09:34:33 +0700

    MinMaxValue

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,6 +1,47 @@
-unsigned long long fibo(long long n)
+#include <iostream>
+#include<cmath>
+using namespace std;
+//kiem tra tang giam hay giam
+bool checktangdangiamdan(long long n) 
 {
-	if (n == 0 || n == 1) // điều kiện dừng
-		return n; // nếu n bằng 0 hoặc bằng 1 thi trả về n
-	else return (fibo(n - 2) + fibo(n - 1));
+	long long d, a, N, S = 0;
+	N = n;
+	while (n > 0) 
+	{
+		d = n % 10;
+		n /= 10;
+		if (d < n % 10) return false;
+	}
+	return true;
+	while (N > 0) 
+	{
+		a = N % 10;
+		N /= 10;
+		if (a > N % 10) return false;
+	}
+	return true;
+}
+// tim chu so min, max
+long long max(long long n) 
+{
+	long long d, max = 0;
+	while (n > 0) 
+	{
+		d = n % 10;
+		if (d > max) max = d;
+		n /= 10;
+	}
+	return max;
+}
+long long min(long long n) 
+{
+	long long d, min;
+	min = n % 10;
+	while (n > 10) 
+	{
+		n /= 10;
+		d = n % 10;
+		if (min > d) min = d;
+	}
+	return min;
 }

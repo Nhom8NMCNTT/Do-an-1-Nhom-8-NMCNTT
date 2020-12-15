54523ec5db51290d5ea4e71922b487a7a01fbc42
Nhom8NMCNTT <74805882+Nhom8NMCNTT@users.noreply.github.com>
2020-12-15 09:31:49 +0700

    Delete header.h

--- header.h
+++
@@ -1,7 +0,0 @@
-unsigned long long fibo(long long x);
-bool checktangdangiamdan(long long n);
-long long max(long long n);
-long long min(long long n);
-long long UCLN(long long a, long long b);
-long long BCNN(long long a, long long b);
-void phantichthuasonguyento(long long n);
